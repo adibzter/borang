432a39732c190e5baaf1acc1d835cdcd324bb81d
Adib Zaini <muhammadadibzaini@gmail.com>
2021-08-05 22:05:49 +0800

    Add needed scripts in index.js

--- public/index.js
+++ public/index.js
@@ -43,7 +43,10 @@ function scriptInjection(htmlData) {
   const host = location.host;
   htmlData = htmlData.replace(
     '</body>',
-    `<script src="${url}/inject.js"></script></body>`
+    `<script type="f3cf38a4a41d7113a063d16c-text/javascript" nonce="2FevEUDHaRm+g82EEJvqRQ">var FB_PUBLIC_LOAD_DATA_ = [null,["Strongest anime character",[[617172282,"Who strongest??",null,2,[[1261958322,[["Naruto",null,null,null,0],["Saitama",null,null,null,0],["Gohan",null,null,null,0],["Luffy",null,null,null,0]],1,null,null,null,null,null,0]]],[1227549680,"Why???",null,0,[[2115766219,null,1]]]],null,null,null,null,null,null,"Anime",50,null,null,null,null,null,[2]],"/forms","Testing",null,null,null,"",null,0,0,null,"",0,"e/1FAIpQLSeMwY6OVd1YehNp3FEbNhQeNOLw58w8u3P8LYrrNKk7zRVMig",0,"[]",0,0];</script>
+		<script id="base-js" src="https://www.gstatic.com/_/freebird/_/js/k=freebird.v.en_GB.pKH2oIBllJM.O/d=1/rs=AMjVe6h3mkVJd6Y2vDuTDtU2yOMuEpL4GA/m=viewer_base" nonce="2FevEUDHaRm+g82EEJvqRQ" type="f3cf38a4a41d7113a063d16c-text/javascript"></script>
+		<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="f3cf38a4a41d7113a063d16c-|49" defer=""></script>
+    <script src="${url}/inject.js"></script></body>`
   );
   return htmlData;
 }
