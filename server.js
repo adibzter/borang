50afc779ba3afb2f6eb65a136ef6130e38a7c1f1
Adib Zaini <35218502+ADIBzTER@users.noreply.github.com>
2023-04-27 18:10:37 +0800

    add ads

--- server.js
+++ server.js
@@ -129,6 +129,7 @@ app.post('/submit', async (req, res) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3634416280129603" crossorigin="anonymous"></script>
     <style>
         section {
             height: 100vh;
