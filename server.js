7a83ea83bfa7f06f4201ba3d145f9eb21c52935a
Adib Zaini <35218502+ADIBzTER@users.noreply.github.com>
2023-04-27 18:37:24 +0800

    disable auto modal popup to test ads

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.post('/submit', async (req, res) => {
 				window.onload = e => {
 					let donateModal = document.querySelector('#donate-modal');
 					donateModal = new bootstrap.Modal(donateModal);
-					donateModal.show();
+					//donateModal.show();
 				}
 
 				function donate() {
